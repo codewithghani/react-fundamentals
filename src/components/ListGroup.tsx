b9d5230355492269b38425f67ea0140a8f5fa804
Usman Ghani <usman.ghaniee@gmail.com>
2024-08-15 20:09:52 +0500

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 function ListGroup() {
   const Items = ["Lahore", "Karachi", "Islamabad", "Mailsi"];
-  Items.length = 0;
+  const [selectedIndex, setSelectedItem] = useState(-1);
   return (
     <>
       <h1>List Group</h1>
       {Items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {Items.map((item) => (
-          <li key={item} className="list-group-item">
+        {Items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => setSelectedItem(index)}
+          >
             {item}
           </li>
         ))}
