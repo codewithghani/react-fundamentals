eeaca22405a1ba50bebaf164b5eb272a37088d02
Usman Ghani <usman.ghaniee@gmail.com>
2024-08-16 10:27:12 +0500

    binding event handlers in Class component

--- src/components/ClassClick.tsx
+++ src/components/ClassClick.tsx
@@ -1,14 +1,30 @@
 import { Component } from "react";
 
 class ClassClick extends Component {
-  handleClick() {
-    console.log("zara zara was clicked");
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      message: "Hello World",
+    };
+    // this.handleClick = this.handleClick.bind(this); binding in the constructor
   }
+
+  handleClick = () => {
+    this.setState({
+      message: "Thank You for Subscription",
+    });
+    // checking this
+    console.log(this);
+  };
   render() {
     return (
       <div>
+        <h1>{this.state.message}</h1>
+        {/* <button onClick={this.handleClick.bind(this)} type="button">   binding inside the return statement ... for heavy applications, this binding method is not recommended */}
+        {/* <button onClick={() => this.handleClick()} type="button"> using arrow function to bind this  */}
         <button onClick={this.handleClick} type="button">
-          Click Me --- Zara Zara Click me
+          Subscribe
         </button>
       </div>
     );
