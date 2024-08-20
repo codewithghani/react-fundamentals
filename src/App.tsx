8067c42d59130ff8a9262e8caa1e72aafbf235f6
Usman Ghani <usman.ghaniee@gmail.com>
2024-08-20 13:36:41 +0500

    using Interface in TypeScript to define the strcuture of props & tying the props with defined structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import "./App.css";
+import ListGroup from "./components/ListGroup";
 // import NameList from "./components/NameList";
-import Style from "./components/Style"; /* using of style sheet  */
+// import Style from "./components/Style"; /* using of style sheet  */
 
 // import ParentComponent from "./components/ParentComponent";
 
 function App() {
+  const Cities = ["Lahore", "Karachi", "Islamabad", "Mailsi"];
+  const Countries = ["Pak", "UK", "USA", "China"];
   return (
     <div>
-      <Style type={true} />
+      <ListGroup items={Cities} heading="Cities" />
+      <ListGroup items={Countries} heading="Countries" />
+
+      {/* <Style type={true} /> */}
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,17 @@
-import { ReactNode, useState } from "react";
-function ListGroup(props: { children: ReactNode; category: string }) {
-  const Items = ["Lahore", "Karachi", "Islamabad", "Mailsi"];
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+}
+function ListGroup({ items, heading }: Props) {
   const [selectedIndex, setSelectedItem] = useState(-1);
   return (
     <>
-      <h1>List Group - {props.category}</h1>
-      {Items.length === 0 && <p>No item found</p>}
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {props.children}
-        {Items.map((item, index) => (
+        {items.map((item, index) => (
           <li
             key={item}
             className={
