8f0a1e390a9c818662b5c9d3b2ae6350fd9c5fb5
Usman Ghani <usman.ghaniee@gmail.com>
2024-08-20 08:49:04 +0500

    adding key prop for map method ... a unique value for each component & not accessible in child component

--- src/components/NameList.tsx
+++ src/components/NameList.tsx
@@ -21,7 +21,9 @@ function NameList() {
       skill: "Vue",
     },
   ];
-  const nameList = persons.map((person) => <Person person={person} />);
+  const nameList = persons.map((person) => (
+    <Person key={person.id} person={person} />
+  ));
   return <div>{nameList}</div>;
 }
 
