63ce367b55d21812763b025b19dc46d0d79eb672
Usman Ghani <usman.ghaniee@gmail.com>
2024-08-22 17:21:02 +0500

    validating react hook form with Zod - a schema based validation apch ... look into documentation of Zod for better usage as well as understanding

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,17 +2,25 @@
 
 // import { FormEvent, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+const schema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Name must be atleast 3 characters long." }),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "You are under age" }),
+});
 
-interface FormData {
-  name: string;
-  age: number;
-}
+type FormData = z.infer<typeof schema>;
+
 const Form = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
   const onSubmit = (data: FieldValues) => {
     console.log(data);
     console.log(errors);
@@ -28,32 +36,25 @@ const Form = () => {
           Name{" "}
         </label>
         <input
-          {...register("name", { required: true, minLength: 3 })}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
         />
-        {errors.name?.type === "required" && (
-          <p className="text-danger"> Name is required</p>
-        )}
-        {errors.name?.type === "minLength" && (
-          <p className="text-danger">
-            {" "}
-            Min Length of Name should be 3 characters long
-          </p>
-        )}
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
           min={0}
         />
+        {errors.age && <p className="text-danger"> {errors.age.message}</p>}
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
