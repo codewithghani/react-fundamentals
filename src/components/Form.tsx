8bbf09c3b803e7333f7582c37d8ef52873e4a223
Usman Ghani <usman.ghaniee@gmail.com>
2024-08-22 16:08:12 +0500

    useRef for accessing input fields & assigning to object for updatiing the value

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,20 @@
 // import React from "react";
 
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = {
+    personName: "",
+    personAge: 0,
+  };
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Form Submitted");
+    if (nameRef.current !== null) person.personName = nameRef.current.value;
+    if (ageRef.current !== null)
+      person.personAge = parseInt(ageRef.current.value);
+    console.log(person);
   };
   return (
     <form
@@ -17,13 +26,19 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name{" "}
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input id="name" type="text" className="form-control" ref={nameRef} />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" min={0} />
+        <input
+          id="age"
+          type="number"
+          className="form-control"
+          min={0}
+          ref={ageRef}
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
