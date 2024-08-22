757e9a1e6e4c796bf93d1b45e976b0f0de68bcb6
Usman Ghani <usman.ghaniee@gmail.com>
2024-08-22 09:56:12 +0500

    implementing shopping cart with quantity change fucntionality & updating the items .... a wow component

--- src/components/UpdatingStateEx.tsx
+++ src/components/UpdatingStateEx.tsx
@@ -1,6 +1,7 @@
 // import React from "react";
 
 import { useState } from "react";
+// import { produce } from "immer";
 
 const UpdatingStateEx = () => {
   const [game, setGame] = useState({
@@ -13,11 +14,50 @@ const UpdatingStateEx = () => {
     name: "Spicy Pepperoni",
     toppings: ["Mushroom"],
   });
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      {
+        id: 1,
+        title: "Product - 1",
+        quantity: 1,
+      },
+      {
+        id: 2,
+        title: "Product - 2",
+        quantity: 1,
+      },
+    ],
+  });
   const handlePlayerChange = () => {
     setGame({ ...game, player: { ...game.player, name: "Faris" } });
   };
   const handleAddTopping = () => {
     setPizza({ ...pizza, toppings: [...pizza.toppings, "Green Olives"] });
+  };
+  const handleDecrement = (bravo: {
+    id: number;
+    title: string;
+    quantity: number;
+  }) => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === bravo.id ? { ...item, quantity: item.quantity - 1 } : item
+      ),
+    });
+  };
+  const handleIncrement = (bravo: {
+    id: number;
+    title: string;
+    quantity: number;
+  }) => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === bravo.id ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
   return (
     <div>
@@ -33,6 +73,28 @@ const UpdatingStateEx = () => {
       <button onClick={handleAddTopping} type="button">
         Add Topping
       </button>
+      <div>
+        <h2>Inc / Dec - Qty in Shopping Cart</h2>
+        <div className="cart--body">
+          <p>Dicount - {cart.discount}</p>
+          <p>
+            {cart.items.map((item) => {
+              return (
+                <p key={item.id}>
+                  {item.title} -{" "}
+                  <button onClick={() => handleDecrement(item)} type="button">
+                    -
+                  </button>{" "}
+                  Qty: {item.quantity}{" "}
+                  <button onClick={() => handleIncrement(item)} type="button">
+                    +
+                  </button>
+                </p>
+              );
+            })}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
