cee211a054cfd7e34877bb1edd9a4ea81464c8a6
Usman Ghani <usman.ghaniee@gmail.com>
2024-08-22 16:57:30 +0500

    Form Validation with react Hook Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,9 +3,20 @@
 // import { FormEvent, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
 const Form = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data: FieldValues) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+    console.log(errors);
+  };
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
@@ -17,11 +28,20 @@ const Form = () => {
           Name{" "}
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger"> Name is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">
+            {" "}
+            Min Length of Name should be 3 characters long
+          </p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
