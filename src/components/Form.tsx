95ac0e3b12d80578152483e514a23d6d3bf2415e
Usman Ghani <usman.ghaniee@gmail.com>
2024-08-22 15:53:49 +0500

    handling submit event & preventing the default submission behavior & logging it into console ...

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,17 @@
 // import React from "react";
 
+import { FormEvent } from "react";
+
 const Form = () => {
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("Form Submitted");
+  };
   return (
-    <form action="" className="container border border-primary rounded px-100">
+    <form
+      onSubmit={handleSubmit}
+      className="container border border-primary rounded px-100"
+    >
       <h2 className="text-center">Basic Form</h2>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
