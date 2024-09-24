c9063200361b3c3386b21d21973d59dfdf63090c
Usman Ghani <usman.ghaniee@gmail.com>
2024-09-24 12:38:45 +0500

    setting up Chkra UI in Vite Projects

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </QueryClientProvider>
   </StrictMode>
 );
