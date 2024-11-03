b16610de5aae6886204f85329a72bc36734acf16
Usman Ghani <usman.ghaniee@gmail.com>
2024-11-03 08:18:53 +0500

    implemnting the complex states using useReducer

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,49 +1,88 @@
-import { Badge, Box, Button, HStack, Text } from "@chakra-ui/react";
+import { Badge, Box, Button, Flex, HStack, Text } from "@chakra-ui/react";
 import { useReducer } from "react";
 
-interface ADDIncrement {
-  type: string;
-}
-interface ADDDecrement {
-  type: string;
-}
-type Action = ADDIncrement | ADDDecrement;
 const Counter = () => {
-  const reducer = (state: number, action) => {
-    switch (action) {
+  const reducer = (state, action) => {
+    switch (action.type) {
       case "INCREMENT":
-        return state + 1;
+        return { ...state, counterone: state.counterone + 1 };
       case "DECREMENT":
-        return state - 1;
+        return { ...state, counterone: state.counterone - 1 };
+      case "INCREMENT2":
+        return { ...state, countertwo: state.countertwo + 1 };
+      case "DECREMENT2":
+        return { ...state, countertwo: state.countertwo - 1 };
       default:
         return state;
     }
   };
-  const [count, dispatch] = useReducer(reducer, 0);
+  const [count, dispatch] = useReducer(reducer, {
+    counterone: 0,
+    countertwo: 10,
+  });
   return (
-    <Box>
-      <HStack>
-        <Text fontSize={"4xl"}>Counter</Text>
-        <Badge paddingX={4} fontSize={"3xl"} colorScheme="teal">
-          {count}
-        </Badge>
-      </HStack>
-      <Button
-        variant={"outline"}
-        colorScheme="green"
-        marginRight={3}
-        onClick={() => dispatch("INCREMENT")}
+    <>
+      <Flex
+        border={"1px solid teal"}
+        direction={"column"}
+        align={"center"}
+        marginBottom={4}
       >
-        Increment
-      </Button>
-      <Button
-        variant={"outline"}
-        colorScheme="red"
-        onClick={() => dispatch("DECREMENT")}
+        <HStack>
+          <Text fontSize={"4xl"}>Counter One: </Text>
+          <Badge paddingX={4} fontSize={"3xl"} colorScheme="teal">
+            {count.counterone}
+          </Badge>
+        </HStack>
+        <HStack>
+          <Button
+            variant={"outline"}
+            colorScheme="green"
+            marginRight={3}
+            onClick={() => dispatch({ type: "INCREMENT" })}
+          >
+            Increment
+          </Button>
+          <Button
+            variant={"outline"}
+            colorScheme="red"
+            onClick={() => dispatch({ type: "DECREMENT" })}
+          >
+            Decrement
+          </Button>
+        </HStack>
+      </Flex>
+      <Flex
+        border={"1px solid dodgerblue"}
+        direction={"column"}
+        align={"center"}
+        background={"grey"}
       >
-        Decrement
-      </Button>
-    </Box>
+        <HStack>
+          <Text fontSize={"4xl"}>Counter Two: </Text>
+          <Badge paddingX={4} fontSize={"3xl"} colorScheme="teal">
+            {count.countertwo}
+          </Badge>
+        </HStack>
+        <HStack>
+          <Button
+            variant={"outline"}
+            colorScheme="green"
+            marginRight={3}
+            onClick={() => dispatch({ type: "INCREMENT2" })}
+          >
+            Increment
+          </Button>
+          <Button
+            variant={"outline"}
+            colorScheme="red"
+            onClick={() => dispatch({ type: "DECREMENT2" })}
+          >
+            Decrement
+          </Button>
+        </HStack>
+      </Flex>
+    </>
   );
 };
 
